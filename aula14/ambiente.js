f4eecf2a6c1c87ef3f0feb218e405fd99029cfed
Gustavo <93099931+GustavoSouzax@users.noreply.github.com>
2024-10-28 17:12:35 -0500

    Exercício de repetições

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+/* 
+var c = 1
+while (c <= 4) {
+    console.log(c)
+    c++
+} 
+*/
+//----------------------//
+
+console.log('Começando...')
+for (var c = 1; c <=4; c++) {
+    console.log(c)
+} 
+console.log('FIM!')

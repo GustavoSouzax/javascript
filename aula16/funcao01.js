b6dc926118fdba13bb8a59cf5be0c02d6da30f32
Gustavo <93099931+GustavoSouzax@users.noreply.github.com>
2024-11-04 08:44:37 -0500

    Exercício de função

---
+++ aula16/funcao01.js
@@ -0,0 +1,21 @@
+/*
+function parimpa(n) {
+    if (n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+console.log(parimpa(19))
+*/
+
+const parimpa = (n) => {
+    if (n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+console.log(parimpa(19))
---
+++ aula16/funcao03.js
@@ -0,0 +1,13 @@
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
+
+// Ou...
+
+let n = (i) => {
+    return i*2
+}
+
+console.log(n(2))
